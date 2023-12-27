05d73d2036054b13a55d82ea89cdc09e9560ceae
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-12-27 11:00:15 +0900

    struct method

--- src/struct_test.rs
+++ src/struct_test.rs
@@ -1,4 +1,12 @@
 pub fn test() {
+    test_user();
+    println!();
+    test_menu();
+    println!();
+    test_rectangle();
+}
+
+fn test_user() {
     let user1 = User {
         email: String::from("someone@exmaple.com"),
         username: String::from("user1"),
@@ -13,6 +21,11 @@ pub fn test() {
         ..user1
     };
     println!("user2 info: {:#?}", user2);
+    user2.username = String::from("UseR2_");
+    user2.email = String::from("user2email@example.com");
+    user2.sign_in_count = 10;
+    user2.active = false;
+    println!("user2 info: {:#?}", user2);
 }
 
 #[derive(Debug)]
@@ -21,4 +34,87 @@ struct User {
     email: String,
     sign_in_count: u64,
     active: bool
+}
+
+
+fn test_menu() {
+    let menu = Menu {
+        name: String::from("test menu"),
+        price: 1000
+    };
+    menu.show();
+
+    let menus: [Menu; 5] = [
+        Menu {
+            name: String::from("chicken"),
+            price: 12000
+        },
+        Menu {
+            name: String::from("pizza"),
+            price: 15000
+        },
+        Menu {
+            name: String::from("burger"),
+            price: 8000
+        },
+        Menu::new(String::from("orange juice"), 3000),
+        Menu::new(String::from("coke"), 2000),
+    ];
+    for menu in menus {
+        menu.show();
+    }
+}
+
+#[derive(Debug)]
+struct Menu {
+    name: String,
+    price: u32
+}
+
+impl Menu {
+    fn new(name: String, price: u32) -> Menu {
+        Menu {
+            name,
+            price
+        }
+    }
+
+    fn show(&self) {
+        println!("{}({})", self.name, self.price);
+    }
+}
+
+
+fn test_rectangle() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(10);
+    println!("square: {:#?}", square);
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
+
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
 }

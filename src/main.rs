80ff772612ac2069271836023e2a2d3f069f702d
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-12-29 09:47:21 +0900

    move tests in 'tests' folder

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-// mod ownership_test;
-// mod reference_test;
-// mod slice_test;
-// mod struct_test;
-// mod enum_test;
-// mod module_test;
-
 mod samples;
 use samples::restaurant;
 
 fn main() {
-    // ownership_test::test();
-    // println!("\n--------------------------------------------\n");
-    // reference_test::test();
-    // println!("\n--------------------------------------------\n");
-    // slice_test::test();
-    // println!("\n--------------------------------------------\n");
-    // struct_test::test();
-    // println!("\n--------------------------------------------\n");
-    // enum_test::test();
-    // println!("\n--------------------------------------------\n");
-    // module_test::test();
     println!("\n--------------------------------------------\n");
     restaurant::test();
 }
---
+++ src/tests/mod.rs
@@ -0,0 +1,20 @@
+pub mod ownership_test;
+pub mod reference_test;
+pub mod slice_test;
+pub mod struct_test;
+pub mod enum_test;
+pub mod module_test;
+
+pub fn test_all() {
+    ownership_test::test();
+    println!("\n--------------------------------------------\n");
+    reference_test::test();
+    println!("\n--------------------------------------------\n");
+    slice_test::test();
+    println!("\n--------------------------------------------\n");
+    struct_test::test();
+    println!("\n--------------------------------------------\n");
+    enum_test::test();
+    println!("\n--------------------------------------------\n");
+    module_test::test();
+}

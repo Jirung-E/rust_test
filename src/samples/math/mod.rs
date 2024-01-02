fd65984d8b16e223225cf91205721e76316a1dac
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-01-02 10:19:03 +0900

    alias

--- src/samples/math/mod.rs
+++ src/samples/math/mod.rs
@@ -1,15 +1,19 @@
 mod point;
 mod vector3;
 
-use point::Point;
+// use point::Point;
+// type Point = vector3::Vector3;
+// use vector3::Vector3 as Point;
+use vector3::Point;
 
 pub fn test() {
-    let p1 = Point::from_i32(10, 2, 12);
+    let mut p1 = Point::from_i32(10, 2, 12);
     let p2 = Point::from_i32(1, 2, 3);
-    let mut p3 = p1 + p2;
-    p3 += p3;
+    p1.x = -8.0;
+    // let mut p3 = p1 + p2;
+    // p3 += p3;
     println!("p1: {:?}", p1);
     println!("p2: {:?}", p2);
-    println!("p3: {:?}", p3);
-    println!("p1 * p2: {:?}", p1 * p2);
+    // println!("p3: {:?}", p3);
+    // println!("p1 * p2: {:?}", p1 * p2);
 }
---
+++ src/samples/math/vector3.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+impl Vector3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vector3 {
+        Vector3 { x, y, z }
+    }
+
+    pub fn from_i32(x: i32, y: i32, z: i32) -> Vector3 {
+        Vector3 {
+            x: x as f64,
+            y: y as f64,
+            z: z as f64
+        }
+    }
+
+    pub fn dot(&self, other: &Vector3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x
+        }
+    }
+}
+
+pub type Point = Vector3;
+// pub use Vector3 as Point;

3da219d029cfdc356ce4ac4c1b5c0cf49e72a267
Jirung-E <jh10590326@gmail.com>
2024-02-23 23:02:50 +0900

    into_iter().filter()

--- src/tests/functional/iterator_test.rs
+++ src/tests/functional/iterator_test.rs
@@ -18,4 +18,35 @@ pub fn test() {
     println!("sum: {}", v1.iter().sum::<i32>());
 
     println!("map(+1): {:?}", v1.iter().map(|x| x+1).collect::<Vec<_>>());
+
+    let shoes = vec![
+        Shoe::new(10, "sneaker"),
+        Shoe::new(13, "sandal"),
+        Shoe::new(10, "boot"),
+    ];
+
+    let in_my_size = shoe_in_size(shoes, 10);
+
+    println!("{:#?}", in_my_size);
+}
+
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String
+}
+
+impl Shoe {
+    fn new(size: u32, style: &str) -> Shoe {
+        Shoe {
+            size,
+            style: style.to_string()
+        }
+    }
+}
+
+
+fn shoe_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }

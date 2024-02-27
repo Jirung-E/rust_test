9a79a62b1943bfc01ee288b28c0d3110ee63777d
Jirung-E <jh10590326@gmail.com>
2024-02-27 23:28:55 +0900

    Box test - Deref trait

--- src/tests/pointer_test/box_test.rs
+++ src/tests/pointer_test/box_test.rs
@@ -15,4 +15,42 @@ pub fn test() {
     println!("b := &a");
     let b = &a;
     println!("b == {}", b);
+
+
+    println!("\n\n");
+
+
+    let x = 5;
+    let y = &x;
+    assert_eq!(x, 5);
+    assert_eq!(*y, 5);
+
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(x, 5);
+    assert_eq!(*y, 5);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(x, 5);
+    assert_eq!(*y, 5);
+}
+
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }

4de384295b5c86604e8ddfaf97bea5d0bd826a06
Jirung-E <jh10590326@gmail.com>
2024-03-03 22:26:52 +0900

    reference cycles, Weak<T>

--- src/tests/pointer_test/mod.rs
+++ src/tests/pointer_test/mod.rs
@@ -2,10 +2,12 @@ mod box_test;
 mod drop_test;
 mod rc_test;
 mod refcell_test;
+mod reference_cycles_test;
+mod weak_test;
 
 
 pub fn test() {
     println!(" [ pointer test ] ");
 
-    refcell_test::test();
+    weak_test::test();
 }

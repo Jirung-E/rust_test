01c16949b6f8dfe538f7d1ff1535388fe37a6100
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-12 10:30:53 +0900

    move

--- src/tests/concurrency_test/mod.rs
+++ src/tests/concurrency_test/mod.rs
@@ -1,20 +1,12 @@
 use std::thread;
-use std::time::Duration;
 
 pub fn test() {
     println!(" [ concurrency test ] ");
 
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("{:?}", v);
     });
-
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
 
     handle.join().unwrap();
 }
